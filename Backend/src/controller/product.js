e1a118e2ddca4123d2f014b7bd64533dd32e594f
sumit4432 <spyadav30897@gmail.com>
2023-04-12 15:44:03 +0530

    FilterMEthode

--- Backend/src/controller/product.js
+++ Backend/src/controller/product.js
@@ -2,6 +2,8 @@ const Category = require("../models/category");
 const Product = require("../models/product");
 const slugify = require("slugify");
 const shortid = require("shortid");
+const category = require("../models/category");
+const product = require("../models/product");
 
 exports.createProduct = async (req, res) => {
   const { name, price, description, category, quantity, createdBy } = req.body;
@@ -34,17 +36,35 @@ exports.createProduct = async (req, res) => {
 
 exports.getProductsBySlug = async (req, res) => {
   try {
-    const products = await Product.find({
-      category: await Category.findOne({ slug: req.params.slug }),
-    })
+    const category = await Category.findOne({ slug: req.params.slug });
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    const products = await Product.find({ category })
       .populate("category")
       .exec();
-    res.status(200).json({
-      products,
-    });
+
+    if (product.length > 0) {
+      res.status(200).json({
+        products,
+        productByPrice: {
+          under5k: products.filter((product) => product.price <= 5000),
+          under10k: products.filter(
+            (product) => product.price > 5000 && product.price <= 10000
+          ),
+          under15k: products.filter(
+            (product) => product.price > 100000 && product.price <= 15000
+          ),
+          under20k: products.filter(
+            (product) => product.price > 15000 && product.price <= 20000
+          ),
+        },
+      });
+    }
   } catch (error) {
     console.log(error);
-    res.status(400).json({
+    res.status(500).json({
       message: "Something went wrong",
     });
   }
